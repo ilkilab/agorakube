cf5ee420d2a5b9dba5ee229b90aa2e265077372a
pierre <43336050+pierreilki@users.noreply.github.com>
2019-08-20 12:14:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ sudo apt update
 sudo apt install -yqq git software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -yqq ansible
-git clone https://github.com/pierreilki/IlkiKube.git
+git clone https://github.com/ilkilab/agorakube.git
